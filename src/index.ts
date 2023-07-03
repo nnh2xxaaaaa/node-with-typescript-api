bf1a23c5f63c4c15c204e251361ba53c6c59d137
nnh2xxaaaaa <ngonhathuy6878@gmail.com>
2023-07-03 18:22:25 +0700

    add style with scss

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,52 @@ import Data from "./api/render";
 import info_user from "./button/Click";
 import { engine } from "express-handlebars";
 import path from "path";
-
+//used global variables
 dotenv.config();
-
+//used express by app
 const app: Express = express();
-const port = process.env.PORT || 8000; // Use process.env.PORT instead of process.env.PORT_8000
+//used port in local server
+const port = process.env.PORT_7000 || 8000; // Use process.env.PORT instead of process.env.PORT_8000
 app.use(express.json());
+// set default settings for handlebars
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+//setting handlebars
+app.set("view engine", "hbs");
 
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
+//setting handlebars
 app.set(
   "views",
   path.join(__dirname, "/resources/views") // Correct the path to views
 );
-
+//test path
 console.log("Path", __dirname + "/resources/views/layouts");
+//create api with static
+app.use(express.static(path.join(__dirname, "public")));
+
+//create api
+app.get("/", (req: Request, res: Response) => {
+  res.send(`<h1>ok</h1>`);
+});
+
+//create api
 app.get("/api", (req: Request, res: Response) => {
   res.send(JSON.stringify(Data));
 });
-
+//create api
 app.get("/api/user", (req: Request, res: Response) => {
   res.send(info_user());
 });
+//create api
 
 app.get("/api/data", (req: Request, res: Response) => {
   res.render("home");
 });
-
+// active api
 app.listen(port, () => {
   console.log(
     `⚡️[server]: Server is running at http://localhost:${port}/api/data`
